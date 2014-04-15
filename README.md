60fac7dc664a51203950e0fbaaff63408e1ceacd
El Atmani Nahim <nahim.elatmani@epita.fr>
2014-04-15 03:28:00 +0200

    Fixing args handling, adding README

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 GMAIL-Generator
 ===============
 
+```
 Generate numerous of valid email that lead to your inbox.
+
+usage: G(en)mail [-h] [-n nb] [-t nb] [-s nb] [-o file.ext] [-f nb] email
+
+G(en)mail generate numerous of valid email that lead directly into your inbox.
+
+positional arguments:
+  email                 email template, since the script is wrote for GMAIL
+                        wejust need the local part of the email
+                        (localpart@domain)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n nb, --number nb    Number of generated email, by default the all
+                        combination will be generated
+  -t nb, --type nb      Disposition preference (0:before|1:mixed|2:after)
+  -s nb, --size nb      max size of the new email, max is 64. Default is the
+                        length of the localpart + 3
+  -o file.ext, --output file.ext
+                        Specify output file, default is stdin
+  -f nb, --format nb    Output format, line by line (0) or CSV (1).Default is
+                        CSV
+```
--- gmailgen.py
+++ gmailgen.py
@@ -18,6 +18,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 """
 
 import argparse
+import sys
 
 MAIL_MAX_LEN    =   64
 PROVIDER        =   'gmail.com'
@@ -28,24 +29,45 @@ parser = argparse.ArgumentParser(description=
         '\ninto your inbox.',
         prog='G(en)mail')
 parser.add_argument('-n', '--number',
-        help    =   'Number of generated email',
+        help    =   'Number of generated email, by default the all' +
+                    ' combination will be generated',
         nargs   =   1,
         default =   0,
+        metavar =   'nb',
         type    =   int,
         dest    =   'number')
 parser.add_argument('-t', '--type',
         help    =   'Disposition preference (0:before|1:mixed|2:after)',
         nargs   =   1,
         default =   0,
+        metavar =   'nb',
         type    =   int,
         choices =   range(0,3),
         dest    =   'type')
+parser.add_argument('-s', '--size',
+        help    =   'max size of the new email, max is 64. Default is the ' +
+                    'length of the localpart + 3',
+        nargs   =   1,
+        default =   0,
+        metavar =   'nb',
+        type    =   int,
+        choices =   range(0,65),
+        dest    =   'size')
 parser.add_argument('-o', '--output',
         help    =   'Specify output file, default is stdin',
         nargs   =   1,
-        default =   'stdin',
+        default =   'stdout',
         metavar =   'file.ext',
         dest    =   'output')
+parser.add_argument('-f', '--format',
+        help    =   'Output format, line by line (0) or CSV (1).' +
+                    'Default is CSV',
+        nargs   =   1,
+        default =   1,
+        metavar =   'nb',
+        type    =   int,
+        choices =   range(0,2),
+        dest    =   'fmt')
 parser.add_argument('email',
         help    =   'email template, since the script is wrote for GMAIL we' +
                     'just need the local part of the email (localpart@domain)',
@@ -55,16 +77,25 @@ parser.add_argument('email',
 args = parser.parse_args()
 
 def main():
+    stdout = sys.stdout if args.output[0] is 'stdout' \
+            else open(args.output[0], 'w')
+    parsemail()
+    maxdot = dotavlb()
+    stdout.close()
+
+def dotavlb():
+    return MAIL_MAX_LEN - len(args.email[0])
+
+def getmails():
+    return mails()
+
+def mails(current, coef, posmail, size):
     pass
 
-def getmail(current, disp, pos, nb):
-    """current is the constructing string, start with \"\"
-    disp is the type of disposition chosen in args
-    pos is the current position in the localpart
-    nb is the remaining address to build"""
-    if nb < 0:
-        pass
-
+def parsemail():
+    posat = args.email[0].find('@')
+    if posat is not -1:
+        args.email[0] = args.email[0][:posat]
 
 if __name__ == '__main__':
     main()
